7174d32702e3f91d2d9fc3dc0d04bbaf83b52a38
Michael Williams <michael.williams@enspiral.com>
2015-10-23 02:16:18 +1300

    enable cors on server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var finalhandler = require('finalhandler')
 var http = require('http')
 var serveStatic = require('serve-static')
- 
+var Cors = require('http-cors')
+
+var cors = new Cors()
 var serve = serveStatic(__dirname)
  
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res)
+
+  if (cors.apply(req, res))
+    return; // this was an OPTIONS request - no further action needed
+
   serve(req, res, done)
 })
  
