255d4694606b1f9865a0a0e9f48c4410cab85737
Michael Williams <michael.williams@enspiral.com>
2017-03-27 14:34:58 +1300

    change server to only redirect

--- server.js
+++ server.js
@@ -1,57 +1,23 @@
-var finalhandler = require('finalhandler')
 var http = require('http')
 var https = require('https')
-var serveStatic = require('serve-static')
-var Cors = require('http-cors')
 var Url = require('url')
+var redirect = require('redirekt')
 
+var home = 'https://www.valueflo.ws'
 var ghPages = {
   '/linked-data-browser': 'https://valueflows.github.io/linked-data-browser'
 }
 
-var cors = new Cors()
-var serve = serveStatic(__dirname)
- 
 var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-
-  if (cors.apply(req, res))
-    return; // this was an OPTIONS request - no further action needed
-
   for (var pageUrl in ghPages) {
     var pageUrlIndex = req.url.indexOf(pageUrl)
     if (pageUrlIndex === 0) {
       var ghPagesUrl = ghPages[pageUrl] + req.url.slice(pageUrl.length)
-      return proxy(ghPagesUrl)(req, res, done)
+      return redirect(req, res, ghPagesUrl)
     }
   }
 
-  serve(req, res, done)
+  redirect(req, res, home)
 })
  
 server.listen(process.env.PORT || 3000)
-
-function proxy (to) {
-  return function (req, res, done) {
-    var urlObj = Url.parse(to)
-
-    var options = {
-      protocol: urlObj.protocol,
-      hostname: urlObj.hostname,
-      port: urlObj.port,
-      method: req.method,
-      path: urlObj.path,
-      headers: req.headers
-    }
-    options.headers.host = options.hostname
-
-    var request = (urlObj.protocol === 'https:' ? https : http).request
-
-    request(options, function (pes) {
-      res.writeHead(pes.statusCode, pes.headers)
-      pes.pipe(res)
-    })
-    .on('error', done)
-    .end()
-  }
-}
